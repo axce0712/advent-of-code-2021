f3d43c778c0e2b202b09f84a1f7b9e2ee5e8099a
Milanovic Christoph <MILANOVC@kapsch.net>
2021-12-03 12:42:32 +0100

    simplifing part two of day 3

--- Day3/PartTwo.fsx
+++ Day3/PartTwo.fsx
@@ -1,28 +1,18 @@
 open System.IO
 
-let numbers =
-    File.ReadLines(Path.Combine(__SOURCE_DIRECTORY__, "input.txt"))
-    |> Seq.toList
+let mostCommon pos bits =
+    let ones, zeros = bits |> List.partition (fun number -> Seq.item pos number = '1')
+    if ones.Length >= zeros.Length then ones else zeros
+    
+let leastCommon pos bits =
+    let ones, zeros = bits |> List.partition (fun number -> Seq.item pos number = '1')
+    if zeros.Length <= ones.Length then zeros else ones
 
-let findOxygenGeneratorRating candidates =
+let findRating determine candidates =
     let rec imp pos left =
         match left with
         | [ number ] -> number
-        | _ ->
-            let ones, zeros = left |> List.partition (fun number -> Seq.item pos number = '1')
-            let newLeft = if ones.Length >= zeros.Length then ones else zeros
-            imp (pos + 1) newLeft
-
-    imp 0 candidates
-
-let findCo2ScrubberRating candidates =
-    let rec imp pos left =
-        match left with
-        | [ number ] -> number
-        | _ ->
-            let ones, zeros = left |> List.partition (fun number -> Seq.item pos number = '1')
-            let newLeft = if zeros.Length <= ones.Length then zeros else ones
-            imp (pos + 1) newLeft
+        | _ -> imp (pos + 1) (determine pos left)
 
     imp 0 candidates
 
@@ -30,15 +20,16 @@ let binaryNumberToInt (input: string) =
     input
     |> Seq.rev
     |> Seq.indexed
-    |> Seq.sumBy (fun (n, x) ->
-        if x = '1'
-        then Seq.replicate n 2 |> Seq.fold (fun a b -> a * b) 1
-        else 0)
+    |> Seq.sumBy (fun (n, x) -> if x = '1' then Seq.replicate n 2 |> Seq.fold (*) 1 else 0)
+
+let numbers =
+    File.ReadLines(Path.Combine(__SOURCE_DIRECTORY__, "input.txt"))
+    |> Seq.toList
 
 let oxygenGeneratorRating =
-    findOxygenGeneratorRating numbers |> binaryNumberToInt
+    findRating mostCommon numbers |> binaryNumberToInt
 
 let co2ScrubberRating =
-    findCo2ScrubberRating numbers |> binaryNumberToInt
+    findRating leastCommon numbers |> binaryNumberToInt
 
 oxygenGeneratorRating * co2ScrubberRating
