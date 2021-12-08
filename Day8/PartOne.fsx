b6fb7636433542f4aa31f106bda0eebd167314e4
Christoph Milanovic <christoph.milanovic@vertretungsnetz.at>
2021-12-08 21:58:44 +0100

    fix demo solution of day8 part one

--- Day8/PartOne.fsx
+++ Day8/PartOne.fsx
@@ -13,29 +13,16 @@ let count (digit : string) =
     | _ -> 0
 
 // let content =
-//     "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb |
-// fdgacbe cefdb cefbgd gcbe
-// edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec |
-// fcgedb cgb dgebacf gc
-// fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef |
-// cg cg fdcagb cbg
-// fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega |
-// efabcd cedba gadfec cb
-// aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga |
-// gecf egdcabf bgf bfgea
-// fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf |
-// gebdcfa ecba ca fadegcb
-// dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf |
-// cefg dcbef fcge gbcadfe
-// bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd |
-// ed bcgafe cdgba cbgef
-// egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg |
-// gbdfcae bgc cg cgb
-// gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc |
-// fgae cfgab fg bagce".Replace("|\n", "| ")
-
-// let digits =
-//     content.Split('\n') |> Seq.map parse |> Seq.toList
+//     "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+// edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+// fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+// fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+// aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+// fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+// dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+// bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+// egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+// gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce".Split('\n')
 
 let content =
     File.ReadLines(Path.Combine(__SOURCE_DIRECTORY__, "input.txt"))
